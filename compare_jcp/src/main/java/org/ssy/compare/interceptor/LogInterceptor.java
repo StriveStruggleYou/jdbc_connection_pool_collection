c7580086c92aaa8ba3b187db730f09bc70ea6e22
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-22 18:26:44 +0800

    新增一个简单的插件

---
+++ compare_jcp/src/main/java/org/ssy/compare/interceptor/LogInterceptor.java
@@ -0,0 +1,61 @@
+package org.ssy.compare.interceptor;
+
+import java.sql.Connection;
+import java.util.Properties;
+import java.util.Set;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
+/**
+ * Created by manager on 2018/6/22.
+ */
+@Intercepts({
+    @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})
+})
+public class LogInterceptor implements Interceptor {
+
+  public Object intercept(Invocation invocation) throws Throwable {
+
+    if (invocation.getTarget() instanceof StatementHandler) {
+      StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+      MetaObject metaStatementHandler = SystemMetaObject.forObject(statementHandler);
+
+      BoundSql boundSql = (BoundSql) metaStatementHandler.getValue("delegate.boundSql");
+
+      String sql = boundSql.getSql();
+      System.out.println("原始SQL: " + sql);
+
+      String pageSql = overrideSQL(sql);
+      System.out.println("改写后的SQL: " + pageSql);
+
+      metaStatementHandler.setValue("delegate.boundSql.sql", pageSql);
+    }
+    return invocation.proceed();
+  }
+
+  public String overrideSQL(String sql) {
+    sql = sql.replace("SELECT", "select");
+    return sql;
+  }
+
+  public Object plugin(Object o) {
+    if (o instanceof StatementHandler) {
+      return Plugin.wrap(o, this);
+    } else {
+      return o;
+    }
+  }
+
+  public void setProperties(Properties properties) {
+
+    System.out.println("setProperties....:" + properties.toString());
+
+  }
+}
