9dccd5b6cdd1aeb24b7dbed19b118a5c91e23eb4
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-25 00:03:45 +0800

    提交基础信息

--- mybatis_learn/src/main/java/org/ssy/mybatis/LearnMybatisInterceptor.java
+++ mybatis_learn/src/main/java/org/ssy/mybatis/LearnMybatisInterceptor.java
@@ -1,16 +1,36 @@
 package org.ssy.mybatis;
 
+import com.google.gson.Gson;
+import java.sql.SQLException;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.ssy.mybatis.common.Business;
+import org.ssy.mybatis.common.BusinessMapper;
+import org.ssy.mybatis.util.DataSourceUtil;
+
 /**
  * Created by manager on 2018/6/24.
  */
 public class LearnMybatisInterceptor {
 
-  private static LearnMybatisInterceptor ourInstance = new LearnMybatisInterceptor();
+  public static void main(String args[]) throws SQLException {
+    SqlSessionFactory sqlSessionFactory = DataSourceUtil.getSqlSessionFactoryInterceptor();
+    SqlSession session = sqlSessionFactory.openSession();
+    try {
+      BusinessMapper mapper = session.getMapper(BusinessMapper.class);
 
-  public static LearnMybatisInterceptor getInstance() {
-    return ourInstance;
+      Business business = mapper.selectBusinessName("'云片2'");
+      Thread.sleep(10000);
+      business = mapper.selectBusiness(1);
+
+      Gson gson = new Gson();
+      System.out.println(gson.toJson(business));
+
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      session.close();
+    }
   }
 
-  private LearnMybatisInterceptor() {
-  }
 }
