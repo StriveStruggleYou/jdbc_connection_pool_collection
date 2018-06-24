3dcf1fa0e37f5493bcbc1ad45a4c4a23e04e2c8e
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-24 19:37:53 +0800

    提交mybatis 学习示例

---
+++ mybatis_learn/src/main/java/org/ssy/mybatis/LearnMybatis.java
@@ -0,0 +1,22 @@
+package org.ssy.mybatis;
+
+import org.apache.ibatis.jdbc.SQL;
+
+/**
+ * Created by manager on 2018/6/24.
+ */
+public class LearnMybatis {
+
+  public static void main(String args[]) {
+
+    SQL sql = new SQL();
+
+    String sqlStr = sql.SELECT("name").FROM("bussiness").toString();
+
+    System.out.println(sqlStr);
+
+
+  }
+
+
+}
